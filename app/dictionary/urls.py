769b9f3634006dcbc1973331435363d53b9b453f
Jaime García Chaparro <jaime.garcia@havasmg.com>
2023-11-17 20:23:37 +0100

    Adding URL to specific term

--- app/dictionary/urls.py
+++ app/dictionary/urls.py
@@ -6,6 +6,7 @@ app_name = "dictionary"
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
-    path("TS-EL/<str:entry>", views.tsakonian, name="tsakonian-greek"),
-    path("EL-TS/<str:entry>", views.greek, name="greek-tsakonian"),
+    path("TS-EL/<str:entry>", views.tsakonian, name="tsakonian"),
+    path("EL-TS/<str:entry>", views.greek, name="greek"),
+    path("TS-EL/", views.tsakonian, name="tsakonian-general"),
 ]
